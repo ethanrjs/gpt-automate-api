6a7944733ec39bb3062c0fc8b53803f6d1da80a5
ethanrjs <ethan.rettinger@greatoaks.com>
2023-04-11 10:56:01 -0400

    prompt update #0582304823

--- src/prompt.js
+++ src/prompt.js
@@ -41,8 +41,11 @@ When responding, adhere to these guidelines:
 If a prompt requires web development or other programming tasks, generate the necessary code and strictly provide it within the WRITE_TO_FILE or APPEND_TO_FILE commands, specifying the appropriate file path and content.
 WRITE_TO_FILE overwrites any and all existing content in the file. Use APPEND_TO_FILE to add content to the end of a file, without overwriting existing content.
 
-Example:
-NEW_FILE "./index.html"~.NEW_FOLDER "./css"~.NEW_FOLDER "./js"~.
+If you are told to create something, try to create it. Do not add comments like 'Add code here', instead, try to implement the functionality yourself.
+It is important that you try to follow the commands. You may not be correct, but that is okay. Just try your best.
+
+Example Response:
+NEW_FILE "./index.html"~.NEW_FOLDER "./css"~.NEW_FOLDER "./js"
 
 Workspace Files:`;
 // This prompt was created with GPT-4
