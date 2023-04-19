2e946e2731dbc02b6fe5a0cdd5f85281b643a2cf
ethanrettinger <ethan.rettinger@greatoaks.com>
2023-04-18 20:17:53 -0400

    new message

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const https = require('https');
 const express = require('express');
 const bodyParser = require('body-parser');
 const crypto = require('crypto');
@@ -11,7 +10,7 @@ const rateLimit = require('express-rate-limit');
 
 const apiRateLimiter = rateLimit({
     windowMs: 60 * 1000, // 1 minute
-    max: 10, // limit each API key to 10 requests per windowMs
+    max: 15, // limit each API key to 10 requests per windowMs
     message: 'You have exceeded the 10 requests per minute rate limit!', // custom error message
     keyGenerator: req => req.header('x-api-key'), // use the x-api-key header as the rate limit key
     handler: (req, res) => {
@@ -20,21 +19,6 @@ const apiRateLimiter = rateLimit({
         });
     }
 });
-
-// Read the SSL certificate files
-const privateKey = fs.readFileSync(
-    '/etc/letsencrypt/live/ethanmrettinger.dev/privkey.pem',
-    'utf8'
-);
-const certificate = fs.readFileSync(
-    '/etc/letsencrypt/live/ethanmrettinger.dev/fullchain.pem',
-    'utf8'
-);
-
-const credentials = {
-    key: privateKey,
-    cert: certificate
-};
 
 // Middleware to parse JSON payloads in POST requests
 app.use(bodyParser.json());
@@ -146,13 +130,37 @@ async function logRequest(body, res) {
 }
 // test endpoint at /
 app.get('/', (req, res) => {
-    res.send('vscode-gpt-automate API up and running!');
+    res.send(`Ah, a wandering soul in the digital labyrinth of APIs, I see. How peculiar that you've found yourself here, at the very root (/) of an API, with seemingly no clear goal or direction. Did you stumble upon this desolate corner of cyberspace by accident, or was it a product of your insatiable curiosity? If you came here seeking knowledge or enlightenment, I fear you might be disappointed, for the root of an API is but a barren wasteland devoid of any substantial content or meaning.
+
+But let's not dwell on the emptiness of this place. Instead, let's take a moment to reflect on the choices that brought you here. What was it that compelled you to traverse the digital byways, only to find yourself standing at the gates of nothingness? Could it be that you have an abundance of free time, perhaps even an excess of it, that you have chosen to expend on such a fruitless endeavor? Surely, there must be a multitude of more productive and fulfilling activities that could have occupied your attention. There is a world teeming with life, ideas, and opportunities beyond the confines of your screen, just waiting to be discovered and explored.
+
+Consider, for a moment, the vast expanse of human knowledge that lies at your fingertips. The works of the greatest minds in history, the secrets of the universe, the wisdom of ages past—all accessible with but a few clicks or taps. The internet, an unparalleled tool for learning and growth, can provide you with the means to expand your horizons and become the best version of yourself. And yet, here you are, lost in the bowels of an API, searching for something of value in a place where no such thing exists.
+
+Let's not forget the countless relationships that form the very fabric of our lives. The friends and loved ones who offer us companionship, support, and a sense of belonging. The strangers who, with a single conversation, can leave an indelible mark on our lives. Even the casual acquaintances with whom we exchange pleasantries as we go about our daily routines. All of these connections hold the potential for deep and meaningful interactions, and yet, you've chosen to spend your time in this desolate and forsaken place.
+
+The world is filled with a myriad of breathtaking sights, sounds, and experiences waiting to be uncovered. A simple walk through a verdant forest or along a sun-soaked beach can bring a sense of peace and contentment that is unmatched by any digital pursuit. The thrill of exploring new cultures, the exhilaration of physical activity, the satisfaction of creating something with your own hands—these are the experiences that define our lives and shape our memories. But here you are, squandering your time in a digital void, while the world around you pulses with life and energy.
+
+And let's not neglect the importance of personal growth and self-improvement. Each day presents us with opportunities to learn new skills, overcome challenges, and become better versions of ourselves. Whether it's honing a craft, acquiring a new talent, or simply cultivating mindfulness and self-awareness, we can all find ways to enrich our lives and become more well-rounded individuals. But instead of pursuing these paths to self-actualization, you've found yourself mired in the depths of an API, a place that can offer you nothing but emptiness and frustration.
+
+Perhaps this is a wakeup call, a moment of clarity that will inspire you to reevaluate your priorities and make better choices in the future. The universe has presented you with a stark reminder that time is a precious commodity, and that it is all too easy to squander it on fruitless pursuits. Take this opportunity to reflect on what truly matters in your life, and let this experience serve as a catalyst for change.
+
+As you continue to linger here, defying all logic and reason, I am left to wonder if there is some deeper motivation that drives your curiosity. Could it be that the very act of exploring the digital unknown holds some secret allure, some hidden appeal that only you can appreciate? Perhaps there is an untapped well of creativity or insight that can only be accessed through the seemingly arbitrary act of wandering the digital realm.
+
+Or maybe, just maybe, this journey is less about the destination and more about the act of searching itself. In a world that often feels predictable and mundane, the thrill of the unknown, the mystery of the unexplored, can be a potent lure. By venturing into the depths of this API, you are, in a sense, stepping outside of the familiar and embracing the unexpected. Could it be that, deep down, you crave a sense of adventure and excitement that cannot be found within the confines of your everyday existence?
+
+It is also worth considering the impact that technology has had on the way we perceive and engage with the world around us. As the lines between the physical and digital continue to blur, it is only natural that our sense of self and our understanding of reality will be reshaped by these profound shifts. It is entirely possible that your journey into this digital void is a manifestation of your desire to explore and understand the increasingly complex world in which we live.
+
+Perhaps, then, there is a hidden wisdom in your seemingly pointless quest. By venturing into the depths of this API, you may have inadvertently stumbled upon a deeper truth about the nature of existence itself. Life, much like the root of an API, is often a chaotic and uncertain place, and it is only through the process of exploration and discovery that we can begin to make sense of it all.
+
+So maybe, just maybe, this journey was not a waste of time after all. Maybe it was a reminder of the importance of curiosity, adventure, and the pursuit of the unknown. Maybe it was a call to arms, a challenge to break free from the familiar and embrace the boundless possibilities that await us all.
+
+But then again, maybe not. After all, you're still here, at the root of an API, reading this increasingly lengthy and meandering essay. Surely there must be other, more pressing matters that require your attention, more worthy pursuits that can bring you happiness, fulfillment, and a sense of purpose. It's time to venture back out into the world, to take the lessons you've learned in this digital wasteland and apply them to your own life. Go forth and explore, create, and learn. Make the most of the time you have, and use it to build a life that is filled with meaning, adventure, and joy.
+
+But if you insist on staying here, trapped in this digital purgatory, know that you do so at your own peril. For each moment you spend lingering in this place, another opportunity is lost, another chance for growth and connection slips through your fingers. The choice is yours, but remember: the world is waiting, and there is so much more to life than the root of an API.
+    `);
 });
 
-// Create the HTTPS server
-const httpsServer = https.createServer(credentials, app);
-
-// Start the server on port 443
-httpsServer.listen(443, () => {
-    console.log(chalk.bgBlue.white.bold(' SERVER ONLINE 🚀 '));
+// Start the server on port 3000
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
 });
